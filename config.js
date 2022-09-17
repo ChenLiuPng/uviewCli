6b453ceb0380795ee6fe875373391090392d82df
chenliupeng <vclp@163.com>
2022-09-18 07:40:57 +0800

    feat:add uview2-hbx

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 module.exports = {
   // 可选择的框架
-  framwork:['uview1-hbx'],
+  framwork:['uview1-hbx','uview2-hbx'],
   // 框架对应的下载地址
   foramworkUrl:{
-    'uview1-hbx':'git@github.com:ChenLiuPng/uview1-hbx.git'
+    'uview1-hbx':'git@github.com:ChenLiuPng/uview1-hbx.git',
+    'uview2-hbx':'git@github.com:ChenLiuPng/uview2-hbx.git'
   }
 }
